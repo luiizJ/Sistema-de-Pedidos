a4de1109d81d9ba58989ebd8039d3978ed3044b1
luiizJ <luiizevrsj@gmail.com>
2025-01-05 11:15:33 -0300

    AtualizacaoDeMetadadosEexclusaoDeComponentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Structure from "@/components/Structure";
-import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
    <div>
     <Structure />
-    <ThemeToggle/>
    </div>
   );
 }
